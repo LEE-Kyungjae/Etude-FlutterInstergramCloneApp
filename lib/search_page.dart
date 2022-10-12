d94f8f450bec90c7b6b2a3e3a79cbedbd266b397
LEE-Kyungjae <levelchecking@gmail.com>
2022-10-12 10:54:33 +0900

    search

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -10,7 +10,26 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return _buildBody();
   }
 }
+
 //
+Widget _buildBody() {
+  return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        childAspectRatio: 1.0,
+        mainAxisSpacing: 1.0,
+        crossAxisSpacing: 1.0,
+      ),
+      itemCount: 15,
+      itemBuilder: (context, index) {
+        return _buildListItem(context, index);
+      });
+}
+
+Widget _buildListItem(BuildContext context, int index) {
+  String img_src = 'https://play-lh.googleusercontent.com/5e7z5YCt7fplN4qndpYzpJjYmuzM2WSrfs35KxnEw-Ku1sClHRWHoIDSw3a3YS5WpGcI=w480-h960-rw';
+  return Image.network(img_src, fit: BoxFit.cover);
+}
